804d3edf8c465863a0c7a80cbdae94edce5edab6
emilysamantha80 <emilysamantha80@gmail.com>
2017-07-19 10:21:54 -0700

    Added enum extensions

---
+++ ESH/Utility/ExtensionMethods/EnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH.Utility.ExtensionMethods
+{
+    public static class EnumExtensions
+    {
+        public static string ValueString(this Enum value)
+        {
+            return System.Convert.ToInt32(value).ToString();
+        }
+    }
+}
