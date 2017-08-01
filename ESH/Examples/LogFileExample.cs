b9db8baf92900e0a54220c7df9e7718510aef62a
emilysamantha80 <emilysamantha80@gmail.com>
2017-08-01 08:28:41 -0700

    Added simple example for LogFile class

---
+++ ESH/Examples/LogFileExample.cs
@@ -0,0 +1,26 @@
+﻿using ESH.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH.Examples
+{
+    public class LogFileExample
+    {
+        public static void Test()
+        {
+            // Always wrap the log file in a using statement 
+            using (var logFile = new LogFile(logToFile: true, logToConsole: true))
+            {
+                // The log file should show up under the same folder as the executing code, in the Log folder
+                Console.WriteLine("Log Path: " + logFile.LogPath);
+                // The tag prefix for the created log files
+                Console.WriteLine("Log Tag: " + logFile.LogTag);
+                logFile.WriteLine("Log file test");
+
+            }
+        }
+    }
+}
