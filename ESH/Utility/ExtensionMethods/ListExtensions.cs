c0d3a786afeede06b35582ad4319714a75113c92
emilysamantha80 <emilysamantha80@gmail.com>
2017-08-03 08:22:42 -0700

    Added Split() extension method

---
+++ ESH/Utility/ExtensionMethods/ListExtensions.cs
@@ -0,0 +1,49 @@
+﻿// Title: Utility Extension Methods
+// Author: Emily Heiner
+//
+// MIT License
+// Copyright(c) 2017 Emily Heiner (emilysamantha80@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH.Utility.ExtensionMethods
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Splits a list into a list of lists with each list containing a specified number of items
+        /// </summary>
+        /// <typeparam name="T">Type of list</typeparam>
+        /// <param name="chunkLength">Maximum number of items for each list</param>
+        /// <returns>List of lists</returns>
+        public static IEnumerable<List<T>> Split<T>(this List<T> source, int chunkLength)
+        {
+            for (int i = 0; i < source.Count; i += chunkLength)
+            {
+                yield return source.GetRange(i, Math.Min(chunkLength, source.Count - i));
+            }
+        }
+    }
+}
