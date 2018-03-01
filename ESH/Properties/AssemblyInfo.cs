0153a24fa5c23e19557d0947b5075638f60316d5
emilysamantha80 <emilysamantha80@gmail.com>
2018-03-01 09:27:37 -0800

    Updated assembly information

--- ESH/Properties/AssemblyInfo.cs
+++ ESH/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ESH")]
-[assembly: AssemblyDescription("Various utilities for C#")]
+[assembly: AssemblyDescription("Various C# utilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Emily Heiner")]
 [assembly: AssemblyProduct("ESH")]
