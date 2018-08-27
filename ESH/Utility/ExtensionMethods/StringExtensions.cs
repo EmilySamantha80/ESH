78bc5a5939eeb9ce2b7fc295da7f19b29ee7f699
Emily Heiner <emilysamantha80@gmail.com>
2018-08-27 13:21:35 -0700

    Added extension methods to convert between to/from hex strings

--- ESH/Utility/ExtensionMethods/StringExtensions.cs
+++ ESH/Utility/ExtensionMethods/StringExtensions.cs
@@ -33,6 +33,47 @@ namespace ESH.Utility.ExtensionMethods
 {
     public static class StringExtensions
     {
+
+        /// <summary>
+        /// Converts a string to a byte array.
+        /// </summary>
+        /// <returns>A byte array.</returns>
+        /// <param name="input">A string.</param>
+        public static byte[] ToBytes(this string input)
+        {
+            return Encoding.UTF8.GetBytes(input);
+        }
+
+        /// <summary>
+        /// Converts a hex string to a byte array.
+        /// </summary>
+        /// <returns>A byte array.</returns>
+        /// <param name="input">A hex string.</param>
+        public static byte[] HexStringToBytes(this string input)
+        {
+            if (input.Length % 2 == 1)
+                throw new Exception("Hex string must have an even number of digits ");
+            byte[] arr = new byte[input.Length >> 1];
+            for (int i = 0; i < input.Length >> 1; ++i)
+                arr[i] = (byte)((HexValue(input[i << 1]) << 4) + (HexValue(input[(i << 1) + 1])));
+            return arr;
+        }
+
+        /// <summary>
+        /// Returns the hex value for a specified character
+        /// </summary>
+        private static int HexValue(char c)
+        {
+            if (c >= '0' && c <= '9')
+                return c - '0';
+            else if (c >= 'A' && c <= 'F')
+                return c - 'A' + 10;
+            else if (c >= 'a' && c <= 'f')
+                return c - 'a' + 10;
+
+            throw new ArgumentException("Invalid hex digit: " + c);
+        }
+
         /// <summary>
         /// Returns the string representation of an object, or String.Empty if the object is null
         /// </summary>
